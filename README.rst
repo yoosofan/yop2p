64f6efc760a404d635efafa2298d39cfe10fd73c
Ahmad Yoosofan <yoosofan@myfastmail.com>
2018-08-25 21:15:01 +0430

    fixed cmake

--- README.rst
+++ README.rst
@@ -11,6 +11,24 @@ Ideas
 A true peer to peer library for every where even behind firewall and NAT using idea of full nodes with IP, full nodes without IP, routers, and trackers which can be incentivise them 
 for their help to secure the network based on how much they help the system.
 
+Build
+####################################################################################################
 
+Linux
+****************************************************************************************************
+
+cmake .
+make
+
+Run tests
 ====================================================================================================
 
+For each tests in test/ directory an executable file is generated and can be run. For example
+
+./tesp2p
+
+
+cleaning directory
+====================================================================================================
+
+bash clean.sh
--- test/simple01/main.cpp
+++ test/simple01/main.cpp
@@ -9,50 +9,9 @@
  * This file is part of yop2p project
  */
 
-#include<iostream>
-#include<fstream>
-#include<vector>
- 
-class initialize_peers{
-	private:
-	static constexpr char const *  list_of_hardcoded_trackers[]={
-		"http://yoosofan.github.io/trackers.b",
-		"http://yoosofan.kashanu.ac.ir/trackers.b"
-	};
-	static constexpr char const * list_of_hardcoded_full_nodes_ip[]={
-		""
-	};
-	static constexpr char const * list_of_hardcoded_routers_and_trackers[]={
-		""
-	};
-
-	std::vector<std::string> list_of_read_from_file_trackers;
-	std::vector<std::string> list_of_read_from_file_full_nodes_ip;
-	std::vector<std::string> list_of_read_from_file_routers_and_trackers;
-
-	std::string file_name_of_peers_trackers;
-
-	public:
-	initialize_peers(const char*file_name_of_peers_trackers="peers_trakers.b"){
-		this->file_name_of_peers_trackers=file_name_of_peers_trackers;
-		std::fstream peers_tracker_files;
-		peers_tracker_files.open(file_name_of_peers_trackers,std::ios::in|std::ios::binary);
-		bool content_of_file_is_loaded = false;
-		if(!peers_tracker_files.is_open()){
-			peers_tracker_files.open(file_name_of_peers_trackers,std::ios::out|std::ios::binary);
-			if(!peers_tracker_files.is_open()){
-				std::cout<< "error in opening peers_tracker_files"<<std::endl;
-			}
-			else
-				peers_tracker_files.close();
-		}else{
-			//peers_tracker_files.read()
-		}
-
-	}
-};
+#include<yop2p/peer.hpp>
 int main(){
 	initialize_peers ipeers;
-	std::cout<<"hello world"<<end::endl;
+	std::cout<<"hello world"<<std::endl;
 	return 0;
 }
